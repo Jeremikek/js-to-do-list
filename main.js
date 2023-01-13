7657a273b5993cf37d45056889eedac5db498ff6
Jeremikek <jeremikek@gmail.com>
2023-01-13 18:44:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let win = null;
 
 const createWindow = () => {
     win = new BrowserWindow({
+        //frame:false,
         width: 800,
         height: 600,
         resizable: false,
@@ -15,4 +16,16 @@ const createWindow = () => {
     win.loadFile("index.html");
 };
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
