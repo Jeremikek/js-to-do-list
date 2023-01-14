eca2bf59a65ddce460918614eb73a6a2c4fc0b56
Jeremikek <jeremikek@gmail.com>
2023-01-15 02:29:15 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,6 +2,23 @@ const { contextBridge, ipcRenderer } = require("electron");
 const path = require("path");
 const fs = require("fs");
 
+// read save file
+function readSave(){
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "/src/save.json")));
+}
+
 contextBridge.exposeInMainWorld("saveFile", {
-    load: () => JSON.parse(fs.readFileSync(path.join(__dirname, "/src/save.json"))).data,
+    load: () => readSave().data,
+    add: (obj) => {
+        let data = readSave();
+        data.data.push(obj);
+        data = JSON.stringify(data, null, 2);
+        fs.writeFileSync(path.join(__dirname, "/src/save.json"), data);
+    },
+    delete: (index) => {
+        let data = readSave();
+        data.data.splice(index, 1);
+        data = JSON.stringify(data, null, 2);
+        fs.writeFileSync(path.join(__dirname, "/src/save.json"), data);
+    }
 });
