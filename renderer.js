e773cefa06efaaf25fd0fe260677075f2da17190
Jeremikek <jeremikek@gmail.com>
2023-01-15 02:29:18 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,19 +1,48 @@
 const mainDisplay = document.getElementById("mainDisplay");
+const addListBtn = document.getElementById("addListBtn");
+const viewModeBtn = document.getElementById("viewModeBtn");
 
 // Read save.json data
 const data = window.saveFile.load();
 
-// Create checklist from data
+// list model
+function createList(){
+    let list = document.createElement("div");
+    let btnDiv = document.createElement("div");
+    let addBtn = document.createElement("button");
+    let closeBtn =  document.createElement("button");
+
+    list.classList.add("container", "bg-primary-subtle", "rounded-2", "m-0", "p-0");
+    btnDiv.classList.add("bg-primary", "d-flex", "justify-content-between");
+    addBtn.classList.add("btn", "btn-primary");
+    closeBtn.classList.add("btn", "btn-primary");
+
+    //
+    addBtn.innerHTML = "+"
+    closeBtn.innerHTML = "x";
+
+    list.style.height = '300px';
+    list.style.width = '180px';
+
+    btnDiv.appendChild(addBtn);
+    btnDiv.appendChild(closeBtn);
+    list.appendChild(btnDiv);
+
+    closeBtn.addEventListener("click", (e) => {
+        window.saveFile.delete(e.target.closest(".container").getAttribute("test"));
+        e.target.closest(".container").remove();
+    });
+
+    return list;
+};
+
+// Create list from data
 for(let i = 0; i < data.length; i++){
-    let checkList = document.createElement("div");
+    let list = createList();
     let title = document.createElement("p");
     let body = document.createElement("ul");
 
-    checkList.classList.add("container", "bg-primary-subtle", "rounded-2", "m-0");
-
-    checkList.style.height = '300px';
-    checkList.style.width = '180px';
-
+    list.setAttribute("test", i);
     title.innerHTML = data[i].title;
 
     for(let x = 0; x < data[i].items.length; x++){
@@ -21,7 +50,22 @@ for(let i = 0; i < data.length; i++){
         item.innerHTML = data[i].items[x];
         body.appendChild(item);
     }
-    checkList.appendChild(title);
-    checkList.appendChild(body);
-    mainDisplay.appendChild(checkList);
+    list.appendChild(title);
+    list.appendChild(body);
+    mainDisplay.appendChild(list);
+
 }
+
+addListBtn.addEventListener("click", () => {
+    const data = window.saveFile.load();
+    let list = createList();
+    let title = document.createElement("p");
+
+    list.setAttribute("test", data.length);
+    title.innerHTML = "New List"
+
+    list.appendChild(title);
+    mainDisplay.appendChild(list);
+
+    window.saveFile.add({"title": "New List", "items": []});
+});
