2a0729ab2ed529ef952d76e252ccbfcf0dec7196
Jeremikek <jeremikek@gmail.com>
2023-01-15 17:48:59 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -5,8 +5,18 @@ const viewModeBtn = document.getElementById("viewModeBtn");
 // Read save.json data
 const data = window.saveFile.load();
 
+// button model
+function createButton(text = "", className = []){
+    let button = document.createElement("button");
+    button.classList.add("btn", "btn-primary", ...className);
+
+    button.innerHTML = text;
+
+    return button;
+}
+
 // input model
-function createInput(className = []){
+function createInput(text = "", className = []){
     let input = document.createElement("input");
 
     input.classList.add("form-control", ...className);
@@ -14,25 +24,52 @@ function createInput(className = []){
     input.style.background = "none";
     input.style.border= "none";
 
+    input.value = text;
+
     return input;
+}
+
+// task model
+function createTask(text = ""){
+    let itemDiv = document.createElement("div");
+    let checkBox = document.createElement("input");
+    let item = createInput(text);
+    let delBtn = createButton("x");
+
+    itemDiv.classList.add("d-flex", "my-1","align-items-baseline");
+    checkBox.classList.add("form-check-input");
+
+    checkBox.type = "checkbox";
+
+    itemDiv.appendChild(checkBox);
+    itemDiv.appendChild(item);
+    itemDiv.appendChild(delBtn);
+
+    // delete task event
+    delBtn.addEventListener("click", (e) => {
+        window.list.deleteTask(e.target.closest(".container").getAttribute("index"), itemDiv.getAttribute("itemIndex"));
+
+        e.target.parentNode.remove();
+    });
+
+    // edit task event
+    item.addEventListener("focusout", (e) => {
+        window.list.editTask(e.target.closest(".container").getAttribute("index"), itemDiv.getAttribute("itemIndex"), e.target.value);
+    });
+
+    return itemDiv;
 }
 
 // list model
 function createList(){
     let list = document.createElement("div");
     let listFrame = document.createElement("div");
-    let addBtn = document.createElement("button");
-    let title = createInput(["title", "m-1"]);
-    let closeBtn =  document.createElement("button");
+    let addBtn = createButton("+");
+    let title = createInput("New List", ["title", "m-1"]);
+    let closeBtn =  createButton("x");
 
     list.classList.add("container", "bg-primary-subtle", "rounded-2", "m-0", "p-0");
     listFrame.classList.add("bg-primary", "d-flex", "justify-content-between");
-    addBtn.classList.add("btn", "btn-primary");
-    closeBtn.classList.add("btn", "btn-primary");
-
-    addBtn.innerHTML = "+"
-    closeBtn.innerHTML = "x";
-    title.value = "New List";
 
     list.style.height = '400px';
     list.style.width = '230px';
@@ -57,16 +94,14 @@ function createList(){
     // add button event
     addBtn.addEventListener("click", (e) => {
         const data = window.saveFile.load();
-        let task = createInput();
-        let taskDiv = document.createElement("li");
 
-        task.value = "New Task";
-        task.setAttribute("itemIndex", data[e.target.closest(".container").getAttribute("index")].items.length);
+        let itemDiv = createTask("New Task");
+
+        itemDiv.setAttribute("itemIndex", data[e.target.closest(".container").getAttribute("index")].items.length);
 
         window.list.addTask(e.target.closest(".container").getAttribute("index"));
         
-        taskDiv.appendChild(task);
-        e.target.parentNode.nextSibling.firstChild.appendChild(taskDiv);
+        e.target.parentNode.nextSibling.appendChild(itemDiv);
     });
 
     return list;
@@ -77,31 +112,20 @@ for(let i = 0; i < data.length; i++){
     let list = createList();
     let title = list.querySelector(".title");
     let body = document.createElement("div");
-    let tasks = document.createElement("ul");
 
-    body.classList.add("p-1");
-    tasks.classList.add("tasks");
+    body.classList.add("px-2");
 
     list.setAttribute("index", i);
     title.value = data[i].title;
 
     for(let x = 0; x < data[i].items.length; x++){
-        let itemDiv = document.createElement("li");
-        let item = document.createElement("input");
+        let itemDiv = createTask(data[i].items[x]);
 
-        item.classList.add("form-control");
+        itemDiv.setAttribute("itemIndex", x);
 
-        item.style.background = "none";
-        item.style.border = "none";
-
-        item.setAttribute("itemIndex", x);
-        item.value = data[i].items[x];
-
-        itemDiv.appendChild(item);
-        tasks.appendChild(itemDiv);
+        body.appendChild(itemDiv);
     }
 
-    body.appendChild(tasks);
     list.appendChild(body);
     mainDisplay.appendChild(list);
 }
@@ -109,11 +133,13 @@ for(let i = 0; i < data.length; i++){
 addListBtn.addEventListener("click", () => {
     const data = window.saveFile.load();
     let list = createList();
-    let title = document.createElement("p");
+    let body = document.createElement("div");
+
+    body.classList.add("px-2");
 
     list.setAttribute("index", data.length);
 
-    list.appendChild(title);
+    list.appendChild(body);
     mainDisplay.appendChild(list);
 
     window.saveFile.add({"title": "New List", "items": []});
